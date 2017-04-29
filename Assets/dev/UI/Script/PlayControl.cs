bfa8d24abd2ff00a51b116b63a7440ed4d8f812c
Yelly K <an.an.yelly@gmail.com>
2017-04-29 16:16:22 +0700

    init file system

---
+++ Assets/dev/UI/Script/PlayControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PlayControl : MonoBehaviour {
+
+	public GameObject GameModePanel;
+	public GameObject Shadow;
+	public int GameMode;
+	public bool isClicked;
+
+	public void Start () {
+		isClicked = false;
+		GameMode = 0;
+
+	}
+
+	public void Update () {
+		Shadow.SetActive (isClicked);
+		GameModePanel.SetActive (isClicked);
+
+	}
+
+	public void switchClick () {
+		if (isClicked)
+			isClicked = false;
+		else
+			isClicked = true;
+	}
+		
+	public void SingleClick() {
+		GameMode = 1;
+		Debug.Log (GameMode);
+	}
+
+	public void MultiClick() {
+		GameMode = 2;
+		Debug.Log (GameMode);
+	}
+
+}
---
+++ Assets/dev/UI/Script/QuitControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class QuitControl : MonoBehaviour {
+
+	public GameObject QuitPanel;
+	public GameObject Shadow;
+	public bool isQuit;
+
+	public void Start () {
+		isQuit = false;
+
+	}
+
+	public void Update () {
+		QuitPanel.SetActive (isQuit);
+		Shadow.SetActive (isQuit);
+	
+	}
+
+	public void switchQuit () {
+		if (isQuit)
+			isQuit = false;
+		else
+			isQuit = true;
+	}
+
+	public void DoQuit () {
+		Debug.Log ("Quit Game");
+		Application.Quit ();
+		//UnityEditor.EditorApplication.Exit (0);
+	}
+
+
+}
