d8c0b4c8e2ca4a79fd3388a72b9a5bed7b386cbc
r3nniw <r3nniw@Krikchais-MacBook-Pro.local>
2017-05-27 20:30:56 +0700

    Essentials Animation : เสร็จครบ

---
+++ Assets/dev/Mechanics/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+  public float movementSpeed;
+  public float jumpForces;
+  public LayerMask groundLayer;
+  public Transform groundCheckerTransform;
+
+  float currentFlip = 1;
+  bool grounded = true;
+  int groundLayerHash;
+
+  // Inputs
+    float horizontal;
+    bool run;
+    bool fire;
+    bool jump;
+
+  // Dependencies
+    Animator animator;
+    Rigidbody2D rb2d;
+
+  void Start() {
+    animator = GetComponent<Animator>();
+    rb2d = GetComponent<Rigidbody2D>();
+
+    groundLayerHash = groundLayer.GetHashCode();
+  }
+
+  void Update() {
+    ManageInputs();
+    AnimatorSetVariables();
+  }
+
+  void FixedUpdate() {
+    HorizontalMove();
+    VerticalMove();
+  }
+
+  // Functions
+    // TODO: กลับมาเปลี่ยนค่า argument ที่ 2 ให้ตรงกับ mechanic จริงๆ
+    void AnimatorSetVariables() {
+      animator.SetBool("isMoving", horizontal != 0.0f);
+      animator.SetBool("isRunning", run);
+      animator.SetBool("isFiring", fire); 
+      animator.SetBool("isGrounded", grounded);
+    }
+
+    void ManageInputs() {
+      horizontal = Input.GetAxisRaw("Horizontal");
+      run = Input.GetKey(KeyCode.LeftShift);
+      fire = run ? false : Input.GetKey(KeyCode.Z);
+      jump = Input.GetKey(KeyCode.UpArrow);
+    }
+
+    void HorizontalMove() {
+      rb2d.transform.localScale = new Vector2(
+        horizontal == 0.0f ? currentFlip : (
+          horizontal == -1.0f ? -1.0f : 1.0f
+        ), 1
+      );
+      currentFlip = Mathf.Sign(rb2d.transform.localScale.x);
+
+      rb2d.velocity = new Vector2(
+        horizontal * (run ? 1.5f : 1.0f) * movementSpeed , rb2d.velocity.y
+      );
+    }
+
+    void VerticalMove() {
+      grounded = Physics2D.OverlapBox(
+        groundCheckerTransform.position,
+        new Vector2(1.0f, 0.1f),
+        0.0f,
+        groundLayerHash
+      );
+      rb2d.AddForce(
+         grounded && jump ? new Vector2(0.0f, jumpForces) : Vector2.zero
+         , ForceMode2D.Impulse
+      );
+    }
+
+}
