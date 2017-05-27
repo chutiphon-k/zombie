065ac9d2e56cf0af6c5928a6079fc2209ae6adde
r3nniw <r3nniw@Krikchais-MacBook-Pro.local>
2017-05-28 02:23:36 +0700

    ขึ้นโครง animation script และ mechanic เบื้องต้น

---
+++ Assets/dev/Mechanics/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+  // Dependencies
+    Animator animator;
+
+  void Update () {
+		AnimatorSetVariables();
+		// float step = speed * Time.deltaTime;
+		// GameObject cantonment = GameObject.Find("Cantonment");
+		// transform.position = Vector3.MoveTowards(transform.position, cantonment.transform.position, step);
+
+		// currentPosition = transform.position;
+		// if(currentPosition != oldPosition){
+		// 	NetworkManager.instance.GetComponent<NetworkManager>().CommandEnemyMove(this.name, transform.position);
+		// 	oldPosition = currentPosition;
+		// }
+	}
+
+	void FixedUpdate() {
+		HorizontalMove();
+	}
+
+  // Functions
+		// TODO: หาวิธีการ move และการ attack เพื่อที่จะเอาไปใส่ใน animation ได้
+    void AnimatorSetVariables() {
+
+    }
+
+    void HorizontalMove() {
+
+    }
+
+}
